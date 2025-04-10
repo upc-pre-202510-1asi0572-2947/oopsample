457ec8c14e1eb447b7c6da82aa1032dc7d5ad4b1
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-10 11:34:57 -0500

    feat(resource): add resource type implementation.

---
+++ src/Resource.cpp
@@ -0,0 +1,10 @@
+#include "Resource.h"
+
+Resource::Resource(std::string name, Type type)
+    : name(std::move(name)), isAvailable(true), resourceType(type){}
+
+std::string Resource::getName() const { return name; }
+
+Resource::Type Resource::getResourceType() const { return resourceType; }
+
+
